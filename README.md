0b671cebf7a1e09cbcfc284700e808395fd7f727
MariaLuizaBertin <mluizabsantos@gmail.com>
2026-06-25 18:22:41 -0300

    modificar os codigos da P2

--- README.md
+++ README.md
@@ -8,14 +8,13 @@ Projeto desenvolvido para a disciplina **Organização de Estrutura de Arquivos*
 2. **Índice Ordenado CEP**
 3. **Ordenação Externa - Diversos Blocos**
 4. **Processamento de CSV / Covid**
-5. **Junção Bolsa Família - Interseção com HASH Join** *(em desenvolvimento)*
-6. **CEP indexado com Árvore B - ISAM** *(em desenvolvimento)*
-7. **Pasta de dados**
+5. **CEP indexado com Árvore B - ISAM**
+6. **Pasta de dados**
 
 ## 📊 Arquivos de Dados Originais
 
 Os arquivos utilizados neste projeto são grandes e não podem ser enviados diretamente para o GitHub.  
-Eles devem ser colocados na pasta `dados` após o download.
+Eles devem ser colocados na pasta *dados* após o download.
 
 📥 [Baixar pasta completa de dados](https://1drv.ms/f/c/4e3f140390335b22/IgCmw160OfjaSqiqhbeQM0F9AWTXGiy2EDoYxYabqkyrjug?e=wCGc5S)
 
@@ -39,12 +38,3 @@ Eles devem ser colocados na pasta `dados` após o download.
 1. Clone este repositório:
    ```bash
    git clone https://github.com/MariaLuizaBertin/Organizacao-de-Estruturas-de-Arquivos.git
-
-# 📚 Fonte dos Dados
-
-Os dados de COVID-19 utilizados neste projeto foram obtidos do repositório público do **Our World in Data (OWID)**:
-
-- Our World in Data. *Coronavirus Pandemic (COVID-19)*. Disponível em: [https://ourworldindata.org/coronavirus](https://ourworldindata.org/coronavirus)  
-- Dataset completo: [https://github.com/owid/covid-19-data](https://github.com/owid/covid-19-data)
-
-Este dataset reúne informações globais sobre casos, mortes, vacinação e outros indicadores relacionados à pandemia, cobrindo a maioria dos países e regiões do mundo.
--- processamento_de_CSV_Covid/ProcessaCSV.c
+++ processamento_de_CSV_Covid/ProcessaCSV.c
@@ -3,16 +3,20 @@
 #include <string.h>
 #include "CSVParser.h"
  
-// emcc Teste2.c CSVParser.c -lnoderawfs.js -lnodefs.js
- 
 #define READ_BUF_SIZE 8192
+
+typedef struct _Dados Dados;
+struct _Dados {
+    int numCasos;
+    int numMortes;
+};
  
 void callback(char** cols, int ncols, void* userData) {
-    int* count = (int*) userData;
+    Dados* dados = (Dados*) userData;
  
     if(strcmp(cols[1], "South America")==0){
-        count[0] += atoi(cols[5]); // casos
-        count[1] += atoi(cols[8]); ///mortes
+        dados->numCasos += atoi(cols[5]); // casos
+        dados->numMortes += atoi(cols[8]); ///mortes
     }
 }
 // DESAFIO: numero total de casos e mortes na america do sul
@@ -20,24 +24,25 @@ void callback(char** cols, int ncols, void* userData) {
 // compilar: gcc Teste2.c CSVParser.c
 int main() {    
     int qt;
-    int count[2]={0,0};
+    Dados dados = {0, 0};
     unsigned int total = 0;
     char* buf = (char*) malloc(READ_BUF_SIZE);
+
     CSVParser csv;
     CSVParser_init(&csv);
-    FILE *f = fopen("../dados/owid-covid-data-american-continente.csv","rb");
+    FILE *f = fopen("../dados/owid-covid-data.csv","rb");
     if(f) {
-        qt = fread(buf,1,READ_BUF_SIZE,f);
+        qt = fread(buf, 1, READ_BUF_SIZE, f);
         while(qt > 0) {
            
             total += qt;
-            CSVParser_processLines(&csv,buf,qt,callback,count);
-            qt = fread(buf,1,READ_BUF_SIZE,f);
+            CSVParser_processLines(&csv, buf, qt, callback, &dados);
+            qt = fread(buf, 1, READ_BUF_SIZE, f);
         }
         fclose(f);
-        CSVParser_processLines(&csv,"\n",1,callback,count);
+        CSVParser_processLines(&csv, "\n", 1, callback, &dados);
         printf("Numero total de casos e mortes na America do Sul\n");
-        printf("Casos: %d \nMortes: %d", count[0],count[1]);
+        printf("Casos: %d \nMortes: %d", dados.numCasos, dados.numMortes);
     }
     free(buf);
     return 0;
